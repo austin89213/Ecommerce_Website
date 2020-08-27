e95d786197b203fdecffc6cf1776c06adbacf5d8
Kira <kira89213@gmail.com>
2020-08-27 23:24:45 +0800

    change debug=Flase on production.py

--- Ecommerce_Website/settings/production.py
+++ Ecommerce_Website/settings/production.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
