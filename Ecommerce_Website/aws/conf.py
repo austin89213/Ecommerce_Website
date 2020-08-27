52ce4f25c8e7d17e1d43136dbe14835cbc370cb6
Kira <kira89213@gmail.com>
2020-08-28 03:01:34 +0800

    Changing the AWS conf

--- Ecommerce_Website/aws/conf.py
+++ Ecommerce_Website/aws/conf.py
@@ -4,8 +4,8 @@ import os
 
 AWS_GROUP_NAME = 'Augustine_eCommerce_Group'
 AWS_USER_NAME = 'augustine-ecommerce-user'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID","AKIATWMS6J2PTJE4XFPC")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY","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")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID","AKIATWMS6J2PV4DMXBVG")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY","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")
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
@@ -13,7 +13,7 @@ AWS_QUERYSTRING_AUTH = True
 
 DEFAULT_FILE_STORAGE = 'Ecommerce_Website.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'Ecommerce_Website.aws.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'augustine-ecommerce'
+AWS_STORAGE_BUCKET_NAME = 'austin-ecommerce'
 S3DIRECT_REGION = 'ap-southeast-1'
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
